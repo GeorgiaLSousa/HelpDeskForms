821c49a391632ffdd16caea78c88419ea5f3bdba
CyberBete <elizabete.f@uni9.edu.br>
2026-05-23 01:17:40 -0300

    Finalizando design dos painéis arredondados na tela de Detalhes do Analista.

--- HelpDeskWindowsForms/Program.cs
+++ HelpDeskWindowsForms/Program.cs
@@ -32,6 +32,7 @@ namespace HelpDeskWindowsForms
             // Registrar os formulários para que possam receber injeção de dependência
             services.AddTransient<Login>();
             services.AddTransient<Cadastro>();
+            services.AddTransient<DetalheAnalista>();
 
             ServiceProvider = services.BuildServiceProvider();
 
@@ -52,7 +53,8 @@ namespace HelpDeskWindowsForms
             }
 
             // Iniciar a aplicação resolvendo o formulário inicial da Injeção de Dependências
-            Application.Run(ServiceProvider.GetRequiredService<Login>());
+            // Iniciar a aplicação resolvendo o formulário 'DetalheAnalista' através da injeção de dependência
+            Application.Run(ServiceProvider.GetRequiredService<DetalheAnalista>());
         }
     }
 }
